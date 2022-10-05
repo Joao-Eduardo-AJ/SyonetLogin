f5d9f586bbe2b2475caeb0d4a4459399af3dfa87
João Eduardo Amorim Jardim <joaoeduaj@gmail.com>
2022-10-05 14:38:37 -0300

    feat: created work environment

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    hoverButton: '#4362CF',
+    function: ' #0D2373',
+    700: '#212121',
+    600: '#5B5B5B',
+    500: '#838383',
+    400: '#C0C0C0',
+    300: '#C4C4C4',
+    200: '#EEEEEE',
+    100: '#FFFFFF',
+    danger: '#DB0000',
+    sucess: '#00CB39',
+  },
+} as const;
